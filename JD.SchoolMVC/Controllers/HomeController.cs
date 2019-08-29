4a307f9052a1eedc1f2285527fb0072f94e59376
IT-Evan <Evan.net@163.com>
2019-08-29 17:52:42 +0800

    Initial

---
+++ JD.SchoolMVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JD.SchoolMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using JD.SchoolMVC.Data;
+using JD.SchoolMVC.Models.SchoolViewModels;
+
+namespace JD.SchoolMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly JDSchoolMVCContext _context;
+
+        public HomeController(JDSchoolMVCContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+    }
+}
